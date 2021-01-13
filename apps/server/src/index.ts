4197428494e989dccfa4ee7a0b0973279be1a093
zjh <zjh@zjhdeiMac-Pro.local>
2021-01-13 15:03:40 +0800

    chore(server) swagger-ui

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,8 +2,13 @@ import Koa from 'koa';
 import logger from 'koa-logger';
 import json from 'koa-json';
 import bodyParser from 'koa-bodyparser';
+import yamljs from 'yamljs';
+import { koaSwagger } from 'koa2-swagger-ui';
 
 import { router } from './routes';
+
+const spec = yamljs.load('./swagger-api/api.yaml');
+router.get('/docs', koaSwagger({ routePrefix: false, swaggerOptions: { spec } }));
 
 const app = new Koa();
 
