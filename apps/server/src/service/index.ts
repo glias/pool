8f876a575a3886b6d9025980cb93697b9acddc29
zkzkp <zk.sec@outlook.com>
2021-01-17 13:45:05 +0800

    chore(server): rename orderBuilder.ts to order_builder

--- apps/server/src/service/index.ts
+++ apps/server/src/service/index.ts
@@ -1,4 +1,4 @@
-export { CellCollector, ICellCollector } from './cellCollector';
-export { OrderBuilder } from './orderBuilder';
-export { dexLiquidityPoolService } from './dex_liquidity_pool_service';
-export { DexLiquidityPoolService } from './dex_liquidity_pool_service';
+export {CellCollector, ICellCollector} from './cellCollector';
+export {OrderBuilder} from './order_builder';
+export {dexLiquidityPoolService} from './dex_liquidity_pool_service';
+export {DexLiquidityPoolService} from './dex_liquidity_pool_service';
