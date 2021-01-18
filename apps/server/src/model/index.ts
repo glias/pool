fd9a62cd80aa8b4ce84915b952da711c560bc608
zzkouniya <74169061+zzkouniya@users.noreply.github.com>
2021-01-18 10:03:53 +0800

    chore(token): sudt token configuration file (#11)
    * chore(token): sudt token configuration file
    
    * update
    
    * update
    
    Co-authored-by: zjh <zjh@zjhdeiMac-Pro.local>

--- apps/server/src/model/index.ts
+++ apps/server/src/model/index.ts
@@ -1,3 +1,4 @@
 export { cellConver, Cell, CellOutput, OutPoint, Script } from './cell';
 export { transactionConver, TransactionWithStatus, Transaction, TxStatus, Input, Output, CellDep } from './transaction';
 export { scriptEquals } from './scriptEquals';
+export { TokenTokenHolderFactory, Token, TokenInfo, TokenHolder } from './tokens';
