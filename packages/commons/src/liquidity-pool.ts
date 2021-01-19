d9e025b12f34fb125488e4556e2c9f9910a38822
homu-chain <dex.ckb.fe@gmail.com>
2021-01-19 17:07:10 +0800

    feat(commons): added liquidity model

--- packages/commons/src/liquidity-pool.ts
+++ packages/commons/src/liquidity-pool.ts
@@ -1,7 +1,11 @@
 import { AssetWithBalance, TransactionStatus } from '.';
 
+// the liquidity model
+export type LiquidityModel = 'UNISWAP';
+
 export interface LiquidityInfo {
   poolId: string;
+  model: LiquidityModel;
   // the liquidity provider token corresponding to the liquidity pool
   lpToken: AssetWithBalance;
   // the liquidity assets in the pool
@@ -10,6 +14,7 @@ export interface LiquidityInfo {
 
 export interface LiquidityOrderSummary {
   poolId: string;
+  model: LiquidityModel;
   assets: AssetWithBalance[];
   status: TransactionStatus;
 }
