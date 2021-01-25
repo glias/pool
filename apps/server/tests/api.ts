a8dc5683f41c64ffdf7969fbc5a16120449f9c45
zkzkp <zk.sec@outlook.com>
2021-01-25 16:37:12 +0800

    test(server): create liquidity pool api

--- apps/server/tests/api.ts
+++ apps/server/tests/api.ts
@@ -1,43 +1,97 @@
 import axios from 'axios';
 import { CKB_TYPE_HASH } from '@gliaswap/constants';
 import * as commons from '@gliaswap/commons';
-import { Script, HashType } from '@lay2/pw-core';
+import dotenv from 'dotenv';
+dotenv.config();
+import { TokenHolderFactory } from '../src/model';
+import * as config from '../src/config';
+import CKBComponents from '@nervosnetwork/ckb-sdk-core/lib';
 
-const TEST_USER_LOCK = new Script(
-  '0x0000000000000000000000000000000000000000000000000000000000000001',
-  '0x',
-  HashType.type,
-);
-const TEST_TOKEN_TYPE_SCRIPT = new Script(
-  '0x0000000000000000000000000000000000000000000000000000000000000002',
-  '0x',
-  HashType.type,
-);
-const TEST_TOKEN_TYPE_HASH = TEST_TOKEN_TYPE_SCRIPT.toHash();
-const TEST_POOL_ID = '0x0000000000000000000000000000000000000000000000000000000000000003';
+const AGGRON_LOCK_CODE_HASH = '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8';
+const AGGRON_SUDT_TYPE_CODE_HASH = '0xc5e5dcf215925f7ef4dfaf5f4b4f105bc321c02776d6e7d52a1db3fcd9d011a4';
+const AGGRON_SUDT_TX_HASH = '0xc1b2ae129fad7465aaa9acc9785f842ba3e6e8b8051d899defa89f5508a77958';
+const LIQUIDITY_ORDER_LOCK_CODE_HASH = process.env.LIQUIDITY_ORDER_LOCK_CODE_HASH || '0x000555';
+const SWAP_ORDER_LOCK_CODE_HASH = process.env.SWAP_ORDER_LOCK_CODE_HASH || '0x000666';
+const INFO_TYPE_CODE_HASH = process.env.INFO_TYPE_CODE_HASH || '0x000777';
+const INFO_LOCK_CODE_HASH = process.env.INFO_LOCK_CODE_HASH || '0x000888';
 
-const generateToken = (amount: number, typeHash: string, typeScript: Script) => {
+const LIQUIDITY_ORDER_LOCK_DEP = {
+  outPoint: {
+    txHash: process.env.LIQUIDITY_ORDER_LOCK_DEP_TX_HASH || '0x000111',
+    index: '0x00',
+  },
+  depType: 'code',
+};
+const SWAP_ORDER_LOCK_DEP = {
+  outPoint: {
+    txHash: process.env.SWAP_ORDER_LOCK_DEP_TX_HASH || '0x000222',
+    index: '0x00',
+  },
+  depType: 'code',
+};
+const INFO_TYPE_DEP = {
+  outPoint: {
+    txHash: process.env.INFO_TYPE_DEP_TX_HASH || '0x000333',
+    index: '0x00',
+  },
+  depType: 'code',
+};
+const INFO_LOCK_DEP = {
+  outPoint: {
+    txHash: process.env.INFO_LOCK_DEP_TX_HASH || '0x000444',
+    index: '0x00',
+  },
+  depType: 'code',
+};
+const SUDT_TYPE_DEP = {
+  outPoint: {
+    txHash: process.env.SUDT_TYPE_DEP_TX_HASH || AGGRON_SUDT_TX_HASH,
+    index: '0x00',
+  },
+  depType: 'code',
+};
+const USER_LOCK: CKBComponents.Script = {
+  codeHash: AGGRON_LOCK_CODE_HASH,
+  hashType: 'type',
+  args: process.env.USER_LOCK_ARGS,
+};
+const TOKENS = ['GLIA', 'ckETH', 'ckDAI', 'ckUSDC', 'ckUSDT'];
+const TOKEN_HOLDER = TokenHolderFactory.getInstance();
+
+const ckbToken = (amount: bigint) => {
   return {
     balance: amount.toString(),
-    typeHash,
-    typeScript,
+    typeHash: CKB_TYPE_HASH,
+    typeScript: undefined,
     info: undefined,
   };
 };
 
-async function test() {
-  const req = {
-    tokenAAmount: generateToken(1000, CKB_TYPE_HASH, undefined),
-    tokenBAmount: generateToken(200, TEST_TOKEN_TYPE_HASH, TEST_TOKEN_TYPE_SCRIPT),
-    poolId: TEST_POOL_ID,
-    userLock: TEST_USER_LOCK,
+const generateToken = (amount: bigint, symbol: string) => {
+  const token = TOKEN_HOLDER.getTokenBySymbol(symbol);
+
+  return {
+    balance: amount.toString(),
+    typeHash: token.typeHash,
+    typeScript: token.typeScript,
+    info: undefined,
   };
+};
+
+async function createTestPool(tokenSymbol: string) {
+  const req = {
+    tokenA: ckbToken(0n),
+    tokenB: generateToken(0n, tokenSymbol),
+    userLock: USER_LOCK,
+  };
+
   console.log(req);
 
-  const resp = await axios.post('http://127.0.0.1:3000/v1/liquidity-pool/orders/genesis-liquidity', req);
-  const tx = commons.TransactionHelper.deserializeTransaction(resp.data.tx);
+  const resp = await axios.post('http://127.0.0.1:3000/v1/liquidity-pool/create-test', req);
+  const tx = commons.TransactionHelper.deserializeTransactionToSign(resp.data.tx);
   console.log(tx);
   console.log(resp.data.fee);
+  console.log(resp.data.lpToken);
 }
 
-test();
+createTestPool('GLIA');
