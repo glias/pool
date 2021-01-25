04610bf7fd0912708f567f9d4666f5651d6bfcb6
yyy-dex <yyy.dex.ckb@gmail.com>
2021-01-25 16:10:51 +0800

    fix

--- apps/server/src/service/index.ts
+++ apps/server/src/service/index.ts
@@ -1,5 +1,5 @@
 export * as txBuilder from './txBuilderService';
 export { dexLiquidityPoolService, DexLiquidityPoolService } from './dexLiquidityPoolService';
 export { dexSwapService, DexSwapService } from './dexSwapService';
-export { TokenCellCollectorService } from './tokenCellCollectorService';
+export { TokenCellCollectorService, DefaultTokenCellCollectorService } from './tokenCellCollectorService';
 export { SendTransactionService, sendTransactionService } from './sendTransactionService';
