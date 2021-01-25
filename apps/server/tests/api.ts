e903d8fb7a0859251ba53a731eb3aaa79ebccc18
zkzkp <zk.sec@outlook.com>
2021-01-26 00:08:44 +0800

    feat(server): create pool tx

--- apps/server/tests/api.ts
+++ apps/server/tests/api.ts
@@ -6,17 +6,10 @@ dotenv.config();
 import { TokenHolderFactory } from '../src/model';
 import * as config from '../src/config';
 import CKBComponents from '@nervosnetwork/ckb-sdk-core/lib';
-
-const AGGRON_SECP256K1_DEP = {
-  outPoint: {
-    txHash: '0xf8de3bb47d055cdf460d93a2a6e1b05f7432f9777c8c474abf4eec1d4aee5d37',
-    index: '0x0',
-  },
-  depType: 'dep_group',
-};
+import CKB from '@nervosnetwork/ckb-sdk-core';
 
 const USER_LOCK: CKBComponents.Script = {
-  codeHash: '0x9bd7e06f3ecf4be0f2fcd2188b23f1b9fcc88e5d4b65a8637b17723bbda3cce8',
+  codeHash: config.SECP256K1_LOCK_CODE_HASH,
   hashType: 'type',
   args: process.env.USER_LOCK_ARGS,
 };
