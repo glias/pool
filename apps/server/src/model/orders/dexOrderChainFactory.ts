db8aac041ba24e0f9afb57cfce238d2fc26aea6e
zjh <zjh@zjhdeiMac-Pro.local>
2021-01-28 16:26:42 +0800

    fix(server): match order lock 2

--- apps/server/src/model/orders/dexOrderChainFactory.ts
+++ apps/server/src/model/orders/dexOrderChainFactory.ts
@@ -97,6 +97,12 @@ export class DexOrderChainFactory {
       });
 
       x.transaction.outputs.forEach((output, index) => {
+        // args: user_lock_hash (32 bytes, 0..32) | version (u8, 1 byte, 32..33) | sudtMin (u128, 16 bytes, 33..49) | ckbMin (u64, 8 bytes, 49..57) | info_type_hash_32 (32 bytes, 57..89) | tips (8 bytes, 89..97) | tips_sudt (16 bytes, 97..113)
+        // argsLen = 244
+        if (!this.isSwapOrder && output.lock.args.length !== 244) {
+          return;
+        }
+
         if (type && !scriptEquals.equalsTypeScript(type, output.type)) {
           return;
         }
@@ -104,6 +110,7 @@ export class DexOrderChainFactory {
         if (!scriptEquals.matchLockScriptWapper(lock, output.lock)) {
           return;
         }
+
         const data = x.transaction.outputsData[index];
         const bridgeInfoResult = bridgeInfoMatch ? bridgeInfoMatch.match(x.transaction.hash) : null;
         const isIn = bridgeInfoResult ? bridgeInfoResult.isIn : null;
