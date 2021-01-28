bcac5f6ff14b0bd69bf53456a2f47107ed5a6904
homu-chain <74238261+homu-chain@users.noreply.github.com>
2021-01-28 20:35:44 +0800

    refactor(ui): implement remove liquidity with the hook (#78)
    * fix(ui): replace with the signer when MetaMask account changed
    
    * fix(ui): change the balance to 0 when the wallet is disconnected
    
    * feat(ui): hide transaction list temporarily
    
    * fix(ui): change the balance to 0 when the wallet is disconnected
    
    * refactor(ui): implement remove liquidity with the hook

--- apps/ui/src/components/AssetManager/AssetBalance/TokenTabs.tsx
+++ apps/ui/src/components/AssetManager/AssetBalance/TokenTabs.tsx
@@ -1,35 +1,32 @@
-import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
-import { RadioItem, RadioTabs } from 'components/AssetManager/components/RadioTabs';
-import { TransactionList } from 'components/AssetManager/TransactionList';
+// import { RadioItem, RadioTabs } from 'components/AssetManager/components/RadioTabs';
+// import { TransactionList } from 'components/AssetManager/TransactionList';
 import { AssetList } from 'components/AssetManager/AssetBalance/TokenList';
+import React, { useEffect } from 'react';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
 
 export const TokenTabs: React.FC = () => {
   const { hash } = useLocation();
   const { replace } = useHistory();
   const match = useRouteMatch();
-  const { t } = useTranslation();
-
   const activatedTab = hash.substring(1);
 
-  function setActivatedTab(tab: string) {
-    replace(`${match.url}#${tab}`);
-  }
+  // function setActivatedTab(tab: string) {
+  //   replace(`${match.url}#${tab}`);
+  // }
 
   useEffect(() => {
     if (!activatedTab) replace(`${match.url}#assets`);
   }, [activatedTab, match.url, replace]);
 
-  const tabContent = activatedTab === 'assets' ? <AssetList /> : <TransactionList />;
-  // const tabContent = <AssetList />;
+  // const tabContent = activatedTab === 'assets' ? <AssetList /> : <TransactionList />;
+  const tabContent = <AssetList />;
 
   return (
     <>
-      <RadioTabs style={{ height: '40px' }} onChange={(selected) => setActivatedTab(selected)} value={activatedTab}>
-        <RadioItem key="assets">{t('Assets')}</RadioItem>
-        <RadioItem key="transactions">{t('Transactions')}</RadioItem>
-      </RadioTabs>
+      {/*<RadioTabs style={{ height: '40px' }} onChange={(selected) => setActivatedTab(selected)} value={activatedTab}>*/}
+      {/*  <RadioItem key="assets">{t('Assets')}</RadioItem>*/}
+      {/*  <RadioItem key="transactions">{t('Transactions')}</RadioItem>*/}
+      {/*</RadioTabs>*/}
       {tabContent}
     </>
   );
--- apps/ui/src/contexts/GliaswapAssetContext.tsx
+++ apps/ui/src/contexts/GliaswapAssetContext.tsx
@@ -2,6 +2,7 @@ import { Asset, GliaswapAPI, GliaswapAssetWithBalance } from '@gliaswap/commons'
 import { useWalletAdapter } from 'commons/WalletAdapter';
 import React, { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import { useQuery } from 'react-query';
+import { createAssetWithBalance } from 'suite';
 
 export type RealtimeInfo<T> = {
   // unix timestamp milliseconds
@@ -44,9 +45,17 @@ export const Provider: React.FC<ProviderProps> = (props) => {
   );
 
   useEffect(() => {
-    if (status !== 'success' || !data) return;
+    if (connectStatus !== 'connected') {
+      setAssetsWithBalance({ lastUpdated: Date.now(), value: assetsWithBalance.value.map(createAssetWithBalance) });
+      return;
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [connectStatus]);
+
+  useEffect(() => {
+    if (status !== 'success' || !data || assetsWithBalance.value === data) return;
     setAssetsWithBalance({ lastUpdated: Date.now(), value: data });
-  }, [status, data]);
+  }, [status, data, assetsWithBalance]);
 
   return (
     <AssetManagerContext.Provider value={{ assets: assetsWithBalance, api }}>{children}</AssetManagerContext.Provider>
