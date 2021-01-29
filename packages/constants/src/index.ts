76d1ce336e93a3b4ff5330aa4318ec6c2665236c
zkzkp <zk.sec@outlook.com>
2021-01-29 18:37:33 +0800

    fix(server): wrong reqeust version

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -10,5 +10,5 @@ export const LIQUIDITY_ORDER_CAPACITY = 235n * CKB_DECIMAL;
 export const INFO_CAPACITY = 250n * CKB_DECIMAL;
 export const MIN_POOL_CAPACITY = 186n * CKB_DECIMAL;
 
-export const REQUEST_VERSION = 0;
+export const REQUEST_VERSION = 1;
 export const CKB_TYPE_HASH = `0x${'0'.repeat(64)}`;
