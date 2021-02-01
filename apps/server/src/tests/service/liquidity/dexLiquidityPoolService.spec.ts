d0486e1d563b600285442ee67962ad4783632108
zjh <zjh@zjhdeiMac-Pro.local>
2021-02-01 15:37:43 +0800

    test(server): describe

--- apps/server/src/tests/service/liquidity/dexLiquidityPoolService.spec.ts
+++ apps/server/src/tests/service/liquidity/dexLiquidityPoolService.spec.ts
@@ -6,32 +6,83 @@ import { mockGliaPoolInfo } from './mockData';
 const mockRepository = MockRepositoryFactory.getDexRepositoryInstance();
 const service = new DexLiquidityPoolService(mockRepository);
 
-test('serialized encoding and decoding args', async () => {
-  const lock = {
-    script: {
-      code_hash: INFO_LOCK_CODE_HASH,
-      hash_type: INFO_LOCK_HASH_TYPE,
-      args: '0x',
-    },
-    argsLen: 'any',
-  };
-  mockRepository
-    .mockCollectCells()
-    .resolves([])
-    .withArgs({
-      lock: lock,
-      type: POOL_INFO_TYPE_SCRIPT[0].toLumosScript(),
-    })
-    .resolves([mockGliaPoolInfo])
-    .withArgs({
-      lock: lock,
-      type: POOL_INFO_TYPE_SCRIPT[1].toLumosScript(),
-    })
-    .resolves([mockGliaPoolInfo]);
+import sinon from 'sinon';
+import sinonStubPromise from 'sinon-stub-promise';
+sinonStubPromise(sinon);
 
-  const result = await service.getLiquidityPools();
-  console.log(result);
+describe('/v1/liquidity-pool', () => {
+  const mock = mockRepository.mockCollectCells();
+  beforeEach(async () => {
+    const lock = {
+      script: {
+        code_hash: INFO_LOCK_CODE_HASH,
+        hash_type: INFO_LOCK_HASH_TYPE,
+        args: '0x',
+      },
+      argsLen: 'any',
+    };
 
-  expect(result.length).toEqual(2);
-  expect(result).toEqual([mockGliaPoolInfo, mockGliaPoolInfo]);
+    mock
+      .resolves([])
+      .withArgs({
+        lock: lock,
+        type: POOL_INFO_TYPE_SCRIPT[0].toLumosScript(),
+      })
+      .resolves([mockGliaPoolInfo])
+      .withArgs({
+        lock: lock,
+        type: POOL_INFO_TYPE_SCRIPT[1].toLumosScript(),
+      })
+      .resolves([mockGliaPoolInfo]);
+  });
+
+  afterEach(async function () {
+    mock.restore();
+  });
+
+  it('get /v1/liquidity-pool', async () => {
+    const result = await service.getLiquidityPools();
+
+    expect(result.length).toEqual(2);
+    expect(result).toEqual([
+      service.toPoolInfo(mockGliaPoolInfo, POOL_INFO_TYPE_SCRIPT[0]),
+      service.toPoolInfo(mockGliaPoolInfo, POOL_INFO_TYPE_SCRIPT[1]),
+    ]);
+  });
+});
+
+describe('/v1/liquidity-pool/pool-id', () => {
+  const mock = mockRepository.mockCollectCells();
+  beforeEach(async () => {
+    const lock = {
+      script: {
+        code_hash: INFO_LOCK_CODE_HASH,
+        hash_type: INFO_LOCK_HASH_TYPE,
+        args: '0x',
+      },
+      argsLen: 'any',
+    };
+
+    mock
+      .resolves([])
+      .withArgs({
+        lock: lock,
+        type: POOL_INFO_TYPE_SCRIPT[0].toLumosScript(),
+      })
+      .resolves([mockGliaPoolInfo])
+      .withArgs({
+        lock: lock,
+        type: POOL_INFO_TYPE_SCRIPT[1].toLumosScript(),
+      })
+      .resolves([mockGliaPoolInfo]);
+  });
+
+  afterEach(async function () {
+    mock.restore();
+  });
+
+  it('get /v1/liquidity-pool/pool-id', async () => {
+    const result = await service.getLiquidityPoolByPoolId(POOL_INFO_TYPE_SCRIPT[1].toHash());
+    expect(result).toEqual(service.toPoolInfo(mockGliaPoolInfo, POOL_INFO_TYPE_SCRIPT[1]));
+  });
 });
