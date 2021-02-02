0772daf3f8b20302513f3f570a9c766d5bce6244
zkzkp <zk.sec@outlook.com>
2021-02-02 19:35:34 +0800

    chore(server): remove jest env

--- apps/server/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import path from 'path';
-import dotenv from 'dotenv';
-
-export default function test() {
-  dotenv.config({ path: path.resolve(__dirname, './src/tests/env') });
-}
