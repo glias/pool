072937e73c5726f77d61618759f4c81070e1bc91
homu-chain <dex.ckb.fe@gmail.com>
2021-02-03 15:30:48 +0800

    feat(ui): add a button to jump to explorer

--- apps/ui/src/components/AssetManager/AssetBalance/index.tsx
+++ apps/ui/src/components/AssetManager/AssetBalance/index.tsx
@@ -1,9 +1,12 @@
-import { DownloadOutlined, SendOutlined } from '@ant-design/icons';
+import { DownloadOutlined, EyeOutlined } from '@ant-design/icons';
+import { isCkbNativeAsset } from '@gliaswap/commons';
 import { Button } from 'antd';
 import { BalanceStatus } from 'components/AssetManager/AssetBalance/BalanceStatus';
 import { TokenTabs } from 'components/AssetManager/AssetBalance/TokenTabs';
 import { WalletConnectionStatusHeader } from 'components/AssetManager/AssetManagerHeader';
 import { useAssetManager } from 'components/AssetManager/hooks';
+import { exploreAddress, exploreSudt } from 'envs';
+import { useGliaswap } from 'hooks';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
@@ -11,30 +14,40 @@ import styled from 'styled-components';
 
 const OperationsWrapper = styled.div`
   text-align: center;
-  margin-bottom: 32px;
+  margin-bottom: 16px;
+
+  padding: 4px 32px;
+
+  .ant-btn {
+    margin-bottom: 4px;
+  }
 `;
 
 export const Operations = () => {
   const { t } = useTranslation();
   const { push } = useHistory();
   const { currentAsset } = useAssetManager();
+  const { currentCkbAddress } = useGliaswap();
   const typeHash = currentAsset.typeHash;
+
+  const exploreUrl = isCkbNativeAsset(currentAsset)
+    ? exploreAddress(currentCkbAddress)
+    : exploreSudt(typeHash, currentCkbAddress);
 
   return (
     <OperationsWrapper>
-      <Button
-        icon={<SendOutlined />}
-        style={{ marginRight: '16px', height: '40px' }}
-        onClick={() => push(`/assets/${typeHash}/send`)}
-      >
-        {t('Send')}
-      </Button>
-      <Button
-        icon={<DownloadOutlined />}
-        style={{ height: '40px' }}
-        onClick={() => push(`/assets/${typeHash}/receive`)}
-      >
+      {/*<Button*/}
+      {/*  icon={<SendOutlined />}*/}
+      {/*  style={{ marginRight: '16px', height: '40px' }}*/}
+      {/*  onClick={() => push(`/assets/${typeHash}/send`)}*/}
+      {/*>*/}
+      {/*  {t('Send')}*/}
+      {/*</Button>*/}
+      <Button block icon={<DownloadOutlined />} onClick={() => push(`/assets/${typeHash}/receive`)}>
         <span>{t('Receive')}</span>
+      </Button>
+      <Button block href={exploreUrl} icon={<EyeOutlined />}>
+        {t('View on CKB Explorer')}
       </Button>
     </OperationsWrapper>
   );
