21673914c317e3ea7eb9ed1c3b9c16077de23e2b
homu-chain <dex.ckb.fe@gmail.com>
2021-02-04 21:21:43 +0800

    fix(ui): fix remove liquidity calc error

--- apps/ui/src/hooks/useRemoveLiquidity.ts
+++ apps/ui/src/hooks/useRemoveLiquidity.ts
@@ -1,19 +1,19 @@
-import { CkbAssetWithBalance, Maybe, SerializedTransactionToSignWithFee, TransactionHelper } from '@gliaswap/commons';
+import {
+  CkbAssetWithBalance,
+  Maybe,
+  price,
+  SerializedTransactionToSignWithFee,
+  TransactionHelper,
+} from '@gliaswap/commons';
 import BigNumber from 'bignumber.js';
 import { useGliaswap } from 'hooks/useGliaswap';
 import { useGlobalSetting } from 'hooks/useGlobalSetting';
 import { useLiquidityQuery } from 'hooks/useLiquidityQuery';
 import update from 'immutability-helper';
+import { zip } from 'lodash';
 import { useEffect, useMemo, useState } from 'react';
 import { useQueryClient } from 'react-query';
-import { Amount, BN, createAssetWithBalance } from 'suite';
-
-function calcMinAmountWithSlippage(balance: string, slippage: number) {
-  return BN(balance)
-    .times(1 - slippage)
-    .decimalPlaces(0, BigNumber.ROUND_FLOOR)
-    .toString();
-}
+import { BN, createAssetWithBalance } from 'suite';
 
 interface UseRemoveLiquidityState {
   generateRemoveLiquidityTransaction: () => Promise<SerializedTransactionToSignWithFee>;
@@ -48,17 +48,31 @@ export function useRemoveLiquidity(poolId?: string): UseRemoveLiquidityState {
       return [[] as CkbAssetWithBalance[], placeholder];
     }
 
-    const assets = userLiquidityQuery.data.assets;
-
-    const readyToReceiveAssets = assets.map<CkbAssetWithBalance>((asset) =>
-      update(asset, {
-        balance: () => Amount.fromAsset(asset).value.times(readyToRemoveShare).toString(),
-      }),
-    );
-
-    const readyToRemoveLpToken = update(poolLiquidityQuery.data.lpToken, {
-      balance: (balance) => BN(balance).times(readyToRemoveShare).toString(),
+    const readyToRemoveLpToken = update(userLiquidityQuery.data.lpToken, {
+      balance: (balance) => {
+        return BN(balance).times(readyToRemoveShare).decimalPlaces(0).toString();
+      },
     });
+
+    const readyToReceiveAssets = zip(poolLiquidityQuery.data.assets, userLiquidityQuery.data.assets).map(
+      ([poolAsset, userAsset], i) => {
+        return update(userAsset!, {
+          balance: () => {
+            if (!poolLiquidityQuery.data || !poolAsset) {
+              throw new Error(`cannot find pool liquidity asset: assets[${i}]`);
+            }
+
+            return price
+              .getRemoveLiquidityReceiveAssetAmount(
+                BN(readyToRemoveLpToken.balance),
+                BN(poolAsset.balance),
+                BN(poolLiquidityQuery.data.lpToken.balance),
+              )
+              .toString();
+          },
+        });
+      },
+    );
 
     return [readyToReceiveAssets, readyToRemoveLpToken];
   }, [poolLiquidityQuery.data, readyToRemoveShare, userLiquidityQuery.data]);
@@ -71,17 +85,21 @@ export function useRemoveLiquidity(poolId?: string): UseRemoveLiquidityState {
     if (!userLiquidityQuery.data) throw new Error('The user liquidity is not loaded');
     if (!currentUserLock) throw new Error('Cannot find the user lock, maybe the wallet is connected');
 
-    const { lpToken, poolId, assets } = userLiquidityQuery.data;
+    const { poolId, assets } = userLiquidityQuery.data;
 
     const tx = await api.generateRemoveLiquidityTransaction({
       poolId,
-      lpToken: update(lpToken, {
-        balance: (balance) => BN(balance).times(readyToRemoveShare).decimalPlaces(0).toString(),
-      }),
+      lpToken: readyToRemoveLpToken,
       lock: currentUserLock,
       tips: createAssetWithBalance(assets[0]),
-      assetsWithMinAmount: assets.map((asset) =>
-        update(asset, { balance: (balance) => calcMinAmountWithSlippage(balance, slippage) }),
+      assetsWithMinAmount: readyToReceiveAssets.map((asset) =>
+        update(asset, {
+          balance: (balance) =>
+            BN(balance)
+              .times(1 - slippage)
+              .decimalPlaces(0, BigNumber.ROUND_FLOOR)
+              .toString(),
+        }),
       ),
     });
 
