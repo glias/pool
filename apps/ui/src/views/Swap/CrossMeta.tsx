1648ebe268ce2ccbf83103427cb151b00f992ce9
coldsnap666 <realyuche@gmail.com>
2021-02-05 18:30:01 +0800

    fix(ui): fix faq link

--- apps/ui/src/views/Swap/CrossMeta.tsx
+++ apps/ui/src/views/Swap/CrossMeta.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { MetaContainer } from 'components/MetaContainer';
 import Gift from 'assets/img/gift.png';
-import { SWAP_CELL_ASK_CAPACITY, SWAP_CELL_BID_CAPACITY, README_URL } from 'suite/constants';
+import { SWAP_CELL_ASK_CAPACITY, SWAP_CELL_BID_CAPACITY } from 'suite/constants';
 import i18n from 'i18n';
 import { SwapMode } from './context';
+import { docsFaq } from 'envs';
 
 export const CrossMeta = ({ swapMode, isBid }: { swapMode: SwapMode; isBid: boolean }) => {
   const amount = swapMode === SwapMode.CrossIn ? '142' : !isBid ? SWAP_CELL_ASK_CAPACITY : SWAP_CELL_BID_CAPACITY;
@@ -17,11 +18,7 @@ export const CrossMeta = ({ swapMode, isBid }: { swapMode: SwapMode; isBid: bool
           {i18n.t('swap.swap-modal.cross-meta', {
             amount,
           })}
-          <a
-            href={`${README_URL}#why-lock-my-addtional-ckb-when-i-place-an-order`}
-            target="_blank"
-            rel="noreferrer noopener"
-          >
+          <a href={docsFaq('why-lock-my-addtional-ckb-when-i-make-a-swap')} target="_blank" rel="noreferrer noopener">
             {i18n.t('swap.swap-modal.learn-why')}
           </a>
         </div>
