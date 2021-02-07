5bdab84e35db17970e1269d4de3729b4bdaa25ba
homu-chain <74238261+homu-chain@users.noreply.github.com>
2021-02-07 23:02:09 +0800

    feat(ui): hide the unnecessary tx fee tips (#208)

--- apps/ui/src/views/Pool/PoolDetail/LiquidityOperation/components/TransactionFeeLabel.tsx
+++ apps/ui/src/views/Pool/PoolDetail/LiquidityOperation/components/TransactionFeeLabel.tsx
@@ -1,5 +1,3 @@
-import { InfoCircleOutlined } from '@ant-design/icons';
-import { Tooltip } from 'antd';
 import i18n from 'i18n';
 import React from 'react';
 
@@ -7,9 +5,9 @@ export const TransactionFeeLabel = () => {
   return (
     <span>
       {i18n.t('Transaction fee')}&nbsp;
-      <Tooltip overlay={i18n.t('the transaction fee')}>
-        <InfoCircleOutlined />
-      </Tooltip>
+      {/*<Tooltip overlay={i18n.t('the transaction fee')}>*/}
+      {/*  <InfoCircleOutlined />*/}
+      {/*</Tooltip>*/}
     </span>
   );
 };
