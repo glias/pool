c32c8389647c88facb1b23140d0dd4aade8da19e
zkzkp <zk.sec@outlook.com>
2021-03-05 16:00:41 +0800

    chore(server): token/token configure

--- apps/server/src/config/tokenToken.ts
+++ apps/server/src/config/tokenToken.ts
@@ -1,29 +1,41 @@
 import { HashType } from '@ckb-lumos/base';
 
-// INFO CELL
-export const INFO_TYPE_CODE_HASH =
-  process.env.TOKEN_TOKEN_INFO_TYPE_CODE_HASH || '0x2e44a62e4e447a2ae5acd0ca186a95f25f86d13571f6a177c5658ab0e63591e9';
-export const INFO_TYPE_HASH_TYPE = process.env.TOKEN_TOKEN_INFO_TYPE_HASH_TYPE || 'type';
-
+// INFO TYPE CELL DEP
 export const INFO_TYPE_DEP = {
   outPoint: {
     txHash:
       process.env.TOKEN_TOKEN_INFO_TYPE_DEP_TX_HASH ||
-      '0x86b757df2d9f20c950b6bfeec48349ce7b4c48c9c89575f1ff68cc13cf487fc8',
+      '0x7db6f068b882da408c11ca4366e450d469247076f885cf029034e2f3f0916380',
+    index: '0x0',
+  },
+  depType: 'code',
+};
+
+export const INFO_TYPE_CODE_HASH =
+  process.env.TOKEN_TOKEN_INFO_TYPE_CODE_HASH || '0x9a90f26821d1f7fb018bc54274ad458428105c5fdf0d2bf6e4d028cc9e6db597';
+export const INFO_TYPE_HASH_TYPE = process.env.TOKEN_TOKEN_INFO_TYPE_HASH_TYPE || 'data';
+
+// INFO LOCK CELL DEL
+export const INFO_LOCK_DEP = {
+  outPoint: {
+    txHash:
+      process.env.TOKEN_TOKEN_INFO_LOCK_DEP_TX_HASH ||
+      '0x5c38faaa4a7e00a7735bbcc02b6b98a3a7bae5769a218929ebaf615cddbb7f23',
     index: '0x0',
   },
   depType: 'code',
 };
 
 export const INFO_LOCK_CODE_HASH =
-  process.env.TOKEN_TOKEN_INFO_LOCK_CODE_HASH || '0x74f5bee3f3ebc5ff31dbeb4da1b37099dfde61fe5f251375fe3ca9618542cca2';
+  process.env.TOKEN_TOKEN_INFO_LOCK_CODE_HASH || '0x20405d74b2fe6b7a9ced51e9a8bb7b10d2c78aaca4d996ba20c838395cde74ee';
 export const INFO_LOCK_HASH_TYPE: HashType = <HashType>process.env.TOKEN_TOKEN_INFO_LOCK_HASH_TYPE || 'data';
 
-export const INFO_LOCK_DEP = {
+// LIQUIDITY LOCK CELL DEP
+export const LIQUIDITY_LOCK_DEP = {
   outPoint: {
     txHash:
-      process.env.TOKEN_TOKEN_INFO_LOCK_DEP_TX_HASH ||
-      '0x611833ecfb298026d554ce3a8183d8c31867172b12d1ef81d45d6c1d91f0d5fc',
+      process.env.TOKEN_TOKEN_LIQUIDITY_LOCK_DEP_TX_HASH ||
+      '0x543314996191cb2b525805535111df3c89b6d8202b731e1f69b8500323deeb78',
     index: '0x0',
   },
   depType: 'code',
@@ -31,29 +43,20 @@ export const INFO_LOCK_DEP = {
 
 export const LIQUIDITY_LOCK_CODE_HASH =
   process.env.TOKEN_TOKEN_LIQUIDITY_LOCK_CODE_HASH ||
-  '0x74bfec21398da1990285d70df943b01e84399be1b6cf19e916f72f4e44bdb225';
-export const LIQUIDITY_LOCK_HASH_TYPE = process.env.TOKEN_TOKEN_LIQUIDITY_LOCK_HASH_TYPE || 'type';
+  '0x267389b032bf58c19dcf6162834d7201d89f401989a2f440b19bc3825c6a243d';
+export const LIQUIDITY_LOCK_HASH_TYPE = process.env.TOKEN_TOKEN_LIQUIDITY_LOCK_HASH_TYPE || 'data';
 
-export const LIQUIDITY_LOCK_DEP = {
+// SWAP LOCK CELL DEP
+export const SWAP_LOCK_DEP = {
   outPoint: {
     txHash:
-      process.env.TOKEN_TOKEN_LIQUIDITY_LOCK_DEP_TX_HASH ||
-      '0x74b0d1a9dcae10b3d9f6dd86bdd06abde1df9a0a6a4866afab20bcb3413a56a0',
+      process.env.TOKEN_TOKEN_SWAP_LOCK_DEP_TX_HASH ||
+      '0xb62f95ad44d31fb94eba3708a355a4c1000f6a606a2055597b2050b7e2bda8c7',
     index: '0x0',
   },
   depType: 'code',
 };
 
 export const SWAP_LOCK_CODE_HASH =
-  process.env.TOKEN_TOKEN_SWAP_LOCK_CODE_HASH || '0x9a81903addf2e696bf24ccbf7a28fec191249ae8f678c65fe813c1cea8a331b9';
-export const SWAP_LOCK_HASH_TYPE = process.env.TOKEN_TOKEN_SWAP_LOCK_HASH_TYPE || 'type';
-
-export const SWAP_LOCK_DEP = {
-  outPoint: {
-    txHash:
-      process.env.TOKEN_TOKEN_SWAP_LOCK_DEP_TX_HASH ||
-      '0xc48d1abd6b37b4fcf9525d361e0c528b4c9bcae82e1f6b1bc2743a401aef7206',
-    index: '0x0',
-  },
-  depType: 'code',
-};
+  process.env.TOKEN_TOKEN_SWAP_LOCK_CODE_HASH || '0x14117f559088695f44087729b616b2afe13b3af1c43a0fc68bba338560e9120d';
+export const SWAP_LOCK_HASH_TYPE = process.env.TOKEN_TOKEN_SWAP_LOCK_HASH_TYPE || 'data';
