94c5e31d99a505ccebc11eeb1d640ec62dbd5cb8
zjh <zjh@zjhdeiMac-Pro.local>
2021-04-01 13:17:15 +0800

    fix(server): filter CrossChainOrder

--- apps/server/src/model/orders/dexSwapOrderChain.ts
+++ apps/server/src/model/orders/dexSwapOrderChain.ts
@@ -1,3 +1,4 @@
+import { QueryOptions, ScriptWrapper } from '@ckb-lumos/base';
 import { CKB_TYPE_HASH, MIN_SUDT_CAPACITY } from '@gliaswap/constants';
 import {
   Output,
@@ -7,8 +8,15 @@ import {
   Script,
   Token,
   PoolInfo,
+  scriptEquals,
 } from '..';
-import { CKB_TOKEN_TYPE_HASH, FORCE_BRIDGE_LOCK_HASH_CODE } from '../../config';
+import {
+  BLOCK_NUMBER,
+  CKB_TOKEN_TYPE_HASH,
+  FORCE_BRIDGE_LOCK_HASH_CODE,
+  SWAP_LOCK_CODE_HASH,
+  SWAP_LOCK_HASH_TYPE,
+} from '../../config';
 import * as utils from '../../utils';
 import { BridgeInfo } from '../bridge';
 import { PoolInfoFactory, QuoteBase } from '../pool';
@@ -94,22 +102,38 @@ export class DexSwapOrderChain extends DexOrderChain {
           amountOut.balance = amount.toString();
         } else {
           if (argsData.sudtTypeHash === CKB_TYPE_HASH) {
-            // sudt => ckb
-            amountOut.balance = (
-              this.getLastOrder()
-                .tx.transaction.outputs.filter((x) => this.equalScript(x.lock, this.userLock))
-                .reduce((total, cell) => total + BigInt(cell.capacity), BigInt(0)) - BigInt(227_0000_0000)
-            ).toString();
+            if (this.getOrders().length === 1) {
+              amountOut.balance = CellInfoSerializationHolderFactory.getInstance()
+                .getSwapCellSerialization()
+                .decodeArgs(this.cell.lock.args)
+                .amountOutMin.toString();
+            } else {
+              // sudt => ckb
+              amountOut.balance = (
+                this.getLastOrder()
+                  .tx.transaction.outputs.filter((x) => this.equalScript(x.lock, this.userLock))
+                  .reduce((total, cell) => total + BigInt(cell.capacity), BigInt(0)) - BigInt(227_0000_0000)
+              ).toString();
+            }
           } else {
             // ckb => sudt
-            let amount = BigInt(0);
-            for (let i = 0; i < this.getLastOrder().tx.transaction.outputs.length; i++) {
-              if (this.equalScript(this.getLastOrder().tx.transaction.outputs[i].lock, this.userLock)) {
-                const data = this.getLastOrder().tx.transaction.outputsData[i];
-                amount += CellInfoSerializationHolderFactory.getInstance().getSwapCellSerialization().decodeData(data);
+            if (this.getOrders().length === 1) {
+              amountOut.balance = CellInfoSerializationHolderFactory.getInstance()
+                .getSwapCellSerialization()
+                .decodeArgs(this.cell.lock.args)
+                .amountOutMin.toString();
+            } else {
+              let amount = BigInt(0);
+              for (let i = 0; i < this.getLastOrder().tx.transaction.outputs.length; i++) {
+                if (this.equalScript(this.getLastOrder().tx.transaction.outputs[i].lock, this.userLock)) {
+                  const data = this.getLastOrder().tx.transaction.outputsData[i];
+                  amount += CellInfoSerializationHolderFactory.getInstance()
+                    .getSwapCellSerialization()
+                    .decodeData(data);
+                }
               }
+              amountOut.balance = amount.toString();
             }
-            amountOut.balance = amount.toString();
           }
         }
       }
@@ -237,6 +261,10 @@ export class DexSwapOrderChain extends DexOrderChain {
       return SWAP_ORDER_TYPE.CrossChain;
     }
 
+    if (this.isBridge()) {
+      return SWAP_ORDER_TYPE.CrossChainOrder;
+    }
+
     return SWAP_ORDER_TYPE.Order;
   }
 
@@ -334,9 +362,9 @@ export class DexSwapOrderChain extends DexOrderChain {
   }
 
   filterOrderHistory(): boolean {
-    if (this.getType() === SWAP_ORDER_TYPE.Order || this.getType() === SWAP_ORDER_TYPE.CrossChainOrder) {
+    if (this.getType() === SWAP_ORDER_TYPE.Order) {
       if (
-        FORCE_BRIDGE_LOCK_HASH_CODE === this.getLastOrder().tx.transaction.inputs[0].cellOutput.lock.codeHash
+        this.isBridge()
         // && this.getLastOrder().tx.txStatus.status === 'pending'
       ) {
         return false;
@@ -345,12 +373,8 @@ export class DexSwapOrderChain extends DexOrderChain {
       }
     }
 
-    // if (SWAP_ORDER_TYPE.Order === this.getType()) {
-    //   return true;
-    // }
-
     if (SWAP_ORDER_TYPE.CrossChainOrder === this.getType()) {
-      if (this.getLastOrder().tx.txStatus.status === 'pending') {
+      if (this.getLastOrder().tx.txStatus.status === 'pending' || !this._bridgeInfo) {
         return false;
       }
 
@@ -375,5 +399,9 @@ export class DexSwapOrderChain extends DexOrderChain {
     const typeHash = PoolInfoFactory.sortTypeHash(this.cell.lock.args.slice(0, 66), this.cell.type.toHash());
     const key = `0x${utils.blake2b(typeHash).slice(2, 66)}`;
     return PoolInfoFactory.getTokens(key);
+  }
+
+  private isBridge(): boolean {
+    return FORCE_BRIDGE_LOCK_HASH_CODE === this.getLastOrder().tx.transaction.inputs[0].cellOutput.lock.codeHash;
   }
 }
